2bb576ac968ef958052489514d1187d3d3755c94
Chris <chris.mood92@gmail.com>
2019-06-11 13:10:25 -0500

    Storing Information in Variables

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 // Code goes here
 console.log("Hello World!");
+
+// Use of const or let
+const name = "David";
+let age = 28;
+
+const productName = "ice cream",
+  productType = "food";
+
+console.log("productName", productName);
+console.log("productType", productType);

af09c1d71aede92b8904ad1f5e7931a9d3498c50
Chris <chris.mood92@gmail.com>
2019-06-11 18:04:35 -0500

    Finish getting starter

--- script.js
+++ script.js
@@ -25,3 +25,107 @@ console.log("answer", answer);
 
     Just for demo purposes
 */
+
+// Arrays
+
+const cities = ["NY", "Los angeles", "Chicago"];
+
+console.log("cities", cities);
+
+// if() Statement
+
+const state = "FL";
+let taxPercent = 0;
+
+if (state === "FL") {
+  taxPercent = 7;
+} else if (state === "NY") {
+  taxPercent = 8;
+}
+
+console.log("taxPercent", taxPercent);
+
+// switch() Statement
+
+const stateExample = "TX";
+
+switch (stateExample) {
+  case "NY":
+    console.log("stateExample", stateExample);
+    break;
+
+  case "TX":
+    console.log("stateExample", stateExample);
+    break;
+
+  default:
+    break;
+}
+
+// for() Loop
+
+const testArray = [0, 1, 2, 3, 4];
+
+for (let i = 0; i < testArray.length; i++) {
+  const element = testArray[i];
+
+  console.log("Element", element);
+}
+
+// Functions
+
+function showMessage() {
+  console.log("Hey show message");
+}
+
+showMessage();
+
+function showMessageWithParams(message) {
+  console.log("message", message);
+}
+
+showMessageWithParams("hey show message im passing params");
+
+// Function return value
+
+function triplePlus(value) {
+  const newValue = value + value + value;
+
+  return newValue;
+}
+
+const result = triplePlus(4);
+
+console.log("triplePlus", result);
+
+// Objects
+
+const person = {
+  name: "John",
+  age: 32,
+  partime: false
+};
+
+console.log("person.name", person.name);
+console.log("person.age", person.age);
+console.log("person.partime", person.partime);
+
+// DOM
+
+const paragraph = document.getElementById("paragraph");
+
+const okButton = document.getElementById("ok-button");
+
+okButton.addEventListener("click", function() {
+  paragraph.innerText = "change text";
+});
+
+const title = document.getElementById("title");
+
+title.style.display = "none";
+
+const showTitleButton = document.getElementById("show-title-button");
+
+showTitleButton.addEventListener("click", function() {
+  title.style.display = "block";
+});
